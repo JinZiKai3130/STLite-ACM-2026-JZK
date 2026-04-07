db21dbfbbd4c595805882eb2291315ca00613d61
ZiKai <jinzikai@sjtu.edu.cn>
2026-04-07 20:21:23 +0800

    feat: add the element member

--- map/src/map.hpp
+++ map/src/map.hpp
@@ -5,219 +5,332 @@
 #define SJTU_MAP_HPP
 
 // only for std::less<T>
-#include <functional>
 #include <cstddef>
-#include "utility.hpp"
+#include <functional>
+
 #include "exceptions.hpp"
+#include "utility.hpp"
 
 namespace sjtu {
 
-template<
-    class Key,
-    class T,
-    class Compare = std::less <Key>
-> class map {
- public:
-  /**
-   * the internal type of data.
-   * it should have a default constructor, a copy constructor.
-   * You can use sjtu::map as value_type by typedef.
-   */
-  typedef pair<const Key, T> value_type;
-  /**
-   * see BidirectionalIterator at CppReference for help.
-   *
-   * if there is anything wrong throw invalid_iterator.
-   *     like it = map.begin(); --it;
-   *       or it = map.end(); ++end();
-   */
-  class const_iterator;
-  class iterator {
+template <class Key, class T, class Compare = std::less<Key> >
+class map {
    private:
-    /**
-     * TODO add data members
-     *   just add whatever you want.
-     */
+    typedef pair<const Key, T> value_type;
+    struct Node {
+        value_type data;
+        Node* lc;
+        Node* rc;
+        Node* fa;
+        int height;
+        Node(value_type& element, Node* lc, Node* rc, int h = 0)
+            : data(element), lc(lc), rc(rc), height(h) {
+        }
+    };
+    Node* root;
+    Compare comp;
+    size_t capacity;
+
+    void destroy(Node* cur) {
+        if (cur == nullptr) return;
+        destroy(cur->lc);
+        destroy(cur->rc);
+        delete cur;
+    }
+
+    Node* clone(Node* other) {
+        if (other == nullptr) return nullptr;
+        Node* p = new Node(other->val);
+        p->height = other->height;
+        try {
+            p->lc = clone(other->lc);
+            p->rc = clone(other->rc);
+        } catch (...) {
+            destroy(p);
+            throw;
+        }
+        return p;
+    }
+
    public:
-    iterator() {
-      // TODO
-    }
-
-    iterator(const iterator &other) {
-      // TODO
-    }
+    /**
+     * the internal type of data.
+     * it should have a default constructor, a copy constructor.
+     * You can use sjtu::map as value_type by typedef.
+     */
+    // typedef pair<const Key, T> value_type;
 
     /**
-     * TODO iter++
+     * see BidirectionalIterator at CppReference for help.
+     *
+     * if there is anything wrong throw invalid_iterator.
+     *     like it = map.begin(); --it;
+     *       or it = map.end(); ++end();
      */
-    iterator operator++(int) {}
+    class const_iterator;
+    class iterator {
+       private:
+        /**
+         * TODO add data members
+         *   just add whatever you want.
+         */
+        Node* cur;
 
-    /**
-     * TODO ++iter
-     */
-    iterator &operator++() {}
+       public:
+        iterator() {
+            // TODO
+        }
 
-    /**
-     * TODO iter--
-     */
-    iterator operator--(int) {}
+        iterator(const iterator& other) {
+            // TODO
+        }
 
-    /**
-     * TODO --iter
-     */
-    iterator &operator--() {}
+        /**
+         * TODO iter++
+         */
+        iterator operator++(int) {
+        }
 
-    /**
-     * a operator to check whether two iterators are same (pointing to the same memory).
-     */
-    value_type &operator*() const {}
+        /**
+         * TODO ++iter
+         */
+        iterator& operator++() {
+        }
 
-    bool operator==(const iterator &rhs) const {}
+        /**
+         * TODO iter--
+         */
+        iterator operator--(int) {
+        }
 
-    bool operator==(const const_iterator &rhs) const {}
+        /**
+         * TODO --iter
+         */
+        iterator& operator--() {
+        }
 
-    /**
-     * some other operator for iterator.
-     */
-    bool operator!=(const iterator &rhs) const {}
+        /**
+         * a operator to check whether two iterators are same (pointing to the
+         * same memory).
+         */
+        value_type& operator*() const {
+        }
 
-    bool operator!=(const const_iterator &rhs) const {}
+        bool operator==(const iterator& rhs) const {
+        }
 
-    /**
-     * for the support of it->first.
-     * See <http://kelvinh.github.io/blog/2013/11/20/overloading-of-member-access-operator-dash-greater-than-symbol-in-cpp/> for help.
-     */
-    value_type *operator->() const
-    noexcept {}
-  };
-  class const_iterator {
-    // it should has similar member method as iterator.
-    //  and it should be able to construct from an iterator.
-   private:
-    // data members.
+        bool operator==(const const_iterator& rhs) const {
+        }
+
+        /**
+         * some other operator for iterator.
+         */
+        bool operator!=(const iterator& rhs) const {
+        }
+
+        bool operator!=(const const_iterator& rhs) const {
+        }
+
+        /**
+         * for the support of it->first.
+         * See
+         * <http://kelvinh.github.io/blog/2013/11/20/overloading-of-member-access-operator-dash-greater-than-symbol-in-cpp/>
+         * for help.
+         */
+        value_type* operator->() const noexcept {
+        }
+    };
+    class const_iterator {
+        // it should has similar member method as iterator.
+        //  and it should be able to construct from an iterator.
+       private:
+        // data members.
+        const Node* cur;
+
+       public:
+        const_iterator() {
+            // TODO
+        }
+
+        const_iterator(const const_iterator& other) {
+            // TODO
+        }
+
+        const_iterator(const iterator& other) {
+            // TODO
+        }
+        // And other methods in iterator.
+        // And other methods in iterator.
+        // And other methods in iterator.
+    };
+
    public:
-    const_iterator() {
-      // TODO
+    /**
+     * TODO two constructors
+     */
+    map() {
+        root = nullptr;
     }
 
-    const_iterator(const const_iterator &other) {
-      // TODO
+    map(const map& other) {
+        root(clone(other.root));
+        comp = other.comp;
+        capacity = other.capacity;
     }
 
-    const_iterator(const iterator &other) {
-      // TODO
+    /**
+     * TODO assignment operator
+     */
+    map& operator=(const map& other) {
+        if (this == &other) return *this;
+        Node* tmp = clone(other.root);
+        destroy(root);
+        root = tmp;
+        comp = other.comp;
+        capacity = other.capacity;
+        return *this;
     }
-    // And other methods in iterator.
-    // And other methods in iterator.
-    // And other methods in iterator.
-  };
 
-  /**
-   * TODO two constructors
-   */
-  map() {}
+    /**
+     * TODO Destructors
+     */
+    ~map() {
+        destroy(root);
+        root = nullptr;
+        capacity = 0;
+    }
 
-  map(const map &other) {}
+    /**
+     * TODO
+     * access specified element with bounds checking
+     * Returns a reference to the mapped value of the element with key
+     * equivalent to key. If no such element exists, an exception of type
+     * `index_out_of_bound'
+     */
+    T& at(const Key& key) {
+    }
 
-  /**
-   * TODO assignment operator
-   */
-  map &operator=(const map &other) {}
+    const T& at(const Key& key) const {
+        // return *find(key);
+    }
 
-  /**
-   * TODO Destructors
-   */
-  ~map() {}
+    /**
+     * TODO
+     * access specified element
+     * Returns a reference to the value that is mapped to a key equivalent to
+     * key, performing an insertion if such key does not already exist.
+     */
+    T& operator[](const Key& key) {
+    }
 
-  /**
-   * TODO
-   * access specified element with bounds checking
-   * Returns a reference to the mapped value of the element with key equivalent to key.
-   * If no such element exists, an exception of type `index_out_of_bound'
-   */
-  T &at(const Key &key) {}
+    /**
+     * behave like at() throw index_out_of_bound if such key does not exist.
+     */
+    const T& operator[](const Key& key) const {
+    }
 
-  const T &at(const Key &key) const {}
+    /**
+     * return a iterator to the beginning
+     */
+    iterator begin() {
+    }
 
-  /**
-   * TODO
-   * access specified element
-   * Returns a reference to the value that is mapped to a key equivalent to key,
-   *   performing an insertion if such key does not already exist.
-   */
-  T &operator[](const Key &key) {}
+    const_iterator cbegin() const {
+    }
 
-  /**
-   * behave like at() throw index_out_of_bound if such key does not exist.
-   */
-  const T &operator[](const Key &key) const {}
+    /**
+     * return a iterator to the end
+     * in fact, it returns past-the-end.
+     */
+    iterator end() {
+    }
 
-  /**
-   * return a iterator to the beginning
-   */
-  iterator begin() {}
+    const_iterator cend() const {
+    }
 
-  const_iterator cbegin() const {}
+    /**
+     * checks whether the container is empty
+     * return true if empty, otherwise false.
+     */
+    bool empty() const {
+    }
 
-  /**
-   * return a iterator to the end
-   * in fact, it returns past-the-end.
-   */
-  iterator end() {}
+    /**
+     * returns the number of elements.
+     */
+    size_t size() const {
+    }
 
-  const_iterator cend() const {}
+    /**
+     * clears the contents
+     */
+    void clear() {
+    }
 
-  /**
-   * checks whether the container is empty
-   * return true if empty, otherwise false.
-   */
-  bool empty() const {}
+    /**
+     * insert an element.
+     * return a pair, the first of the pair is
+     *   the iterator to the new element (or the element that prevented the
+     * insertion), the second one is true if insert successfully, or false.
+     */
+    pair<iterator, bool> insert(const value_type& value) {
+    }
 
-  /**
-   * returns the number of elements.
-   */
-  size_t size() const {}
+    /**
+     * erase the element at pos.
+     *
+     * throw if pos pointed to a bad element (pos == this->end() || pos points
+     * an element out of this)
+     */
+    void erase(iterator pos) {
+    }
 
-  /**
-   * clears the contents
-   */
-  void clear() {}
+    /**
+     * Returns the number of elements with key
+     *   that compares equivalent to the specified argument,
+     *   which is either 1 or 0
+     *     since this container does not allow duplicates.
+     * The default method of check the equivalence is !(a < b || b > a)
+     */
+    size_t count(const Key& key) const {
+    }
 
-  /**
-   * insert an element.
-   * return a pair, the first of the pair is
-   *   the iterator to the new element (or the element that prevented the insertion),
-   *   the second one is true if insert successfully, or false.
-   */
-  pair<iterator, bool> insert(const value_type &value) {}
+    /**
+     * Finds an element with key equivalent to key.
+     * key value of the element to search for.
+     * Iterator to an element with key equivalent to key.
+     *   If no such element is found, past-the-end (see end()) iterator is
+     * returned.
+     */
+    iterator find(const Key& key) {
+        Node* cur = root;
+        while (cur != nullptr && cur->data.first != key) {
+            if (cur->data.first > key) {
+                cur = cur->lc;
+            } else
+                cur = cur->rc;
+        }
+        // if (cur == nullptr)
+        //     return nullptr;
+        // else
+        //     return (value_type*)(&cur->data);
+    }
 
-  /**
-   * erase the element at pos.
-   *
-   * throw if pos pointed to a bad element (pos == this->end() || pos points an element out of this)
-   */
-  void erase(iterator pos) {}
-
-  /**
-   * Returns the number of elements with key
-   *   that compares equivalent to the specified argument,
-   *   which is either 1 or 0
-   *     since this container does not allow duplicates.
-   * The default method of check the equivalence is !(a < b || b > a)
-   */
-  size_t count(const Key &key) const {}
-
-  /**
-   * Finds an element with key equivalent to key.
-   * key value of the element to search for.
-   * Iterator to an element with key equivalent to key.
-   *   If no such element is found, past-the-end (see end()) iterator is returned.
-   */
-  iterator find(const Key &key) {}
-
-  const_iterator find(const Key &key) const {}
+    const_iterator find(const Key& key) const {
+        Node* cur = root;
+        while (cur != nullptr && cur->data.first != key) {
+            if (cur->data.first > key) {
+                cur = cur->lc;
+            } else
+                cur = cur->rc;
+        }
+        // if (cur == nullptr)
+        //     return nullptr;
+        // else
+        //     return (value_type*)(&cur->data);
+    }
 };
 
-}
+}  // namespace sjtu
 
 #endif
